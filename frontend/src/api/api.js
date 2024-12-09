2307f993e4f177454c57eb35d78f72fb79864fe8
Ranjit1818 <ranjeetkulkarni18@gmail.com>
2024-12-09 12:43:24 +0530

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create an Axios instance for the backend API
 const apiClient = axios.create({
-  baseURL: "invoice-backend-orcin.vercel.app/api", // Base URL for the backend
+  baseURL: "https://invoice-backend-orcin.vercel.app/api", // Base URL for the backend
   headers: {
     "Content-Type": "application/json",
   },
